99ba25fc22d4f86f7ba20b8a108df7ebf0b97655
Shreya Kolekar <shreyakolekar.official@gmail.com>
2023-10-13 14:31:53 -0500

    String declaration, manipulation and interpolation

---
+++ string.js
@@ -0,0 +1,49 @@
+//  -------------------   backticks : string interpolation  ------------------
+
+// console.log(`Hello my name is ${fname} and my repo count is ${repoCount}`);
+
+
+// -----------     string declaration   -------------------  //
+
+// const gameName = new String('ShreyaK');
+// console.log(gameName);
+// console.log(typeof(gameName));
+// console.log(gameName.length);     //length is a key in the obj gameName
+// console.log(gameName.toUpperCase()); // toUpperCase is a func in the obj gameName
+// console.log(gameName.charAt(2));    // r
+// console.log(gameName.indexOf('y')); //4
+
+//  ---------------   string manipulation   -------------------- //
+
+// const newString = gameName.substring(0,4);
+// console.group(newString); // Shre
+// const slicedString = gameName.slice(-7,4);
+// console.log(slicedString);
+
+
+// ---------------   trim  ------------------   //
+
+// const newOne = '  Shakala Boom  ';
+// console.log(newOne);
+// console.log(newOne.trim());
+
+
+
+// ------------------   replace  ------------------ //
+
+// const url = "https://hitesh.com/hitesh%20choudhary";
+// console.log(url.replace('%20', '-'));
+
+
+// -------------------  finding substring in string  ---------------------  //
+
+// const sample = 'This is an ant';
+// console.log(sample.includes('ant'));  // true
+
+
+// ---------------- split of string;  split(separator,limit)  ------------------------  //
+
+// const temporary = 'shaka-laka-boom-boom';
+// console.log(temporary.split('-'));
+
+
